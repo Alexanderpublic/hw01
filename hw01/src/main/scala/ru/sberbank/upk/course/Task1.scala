e695843af44813b8eb6a0247fc2fb683a2d67643
Alexanderpublic <51701242+Alexanderpublic@users.noreply.github.com>
2022-03-02 08:10:40 +0300

    renaming + new folders

---
+++ hw01/src/main/scala/ru/sberbank/upk/course/Task1.scala
@@ -0,0 +1,9 @@
+package ru.sberbank.upk.course
+
+object Task1 {
+  def main(args: Array[String]): Unit = {
+    val name = s"Александр"
+    val surname = s"Слободчиков"
+    println(s"Привет, меня зовут $name $surname. Рад познакомиться!")
+  }
+}
